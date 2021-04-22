f1d10d5cea0cd08d9e5be1e982dbfdbd2ef4c2d7
Prof. Celso <celsofurtado@terra.com.br>
2021-04-22 11:29:31 -0300

    Aula 22-04-2021

---
+++ app/src/main/java/br/senai/sp/jandira/imcapp20_a/ResultadoImcActivity.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.imcapp20_a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoImcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_imc)
+    }
+}
